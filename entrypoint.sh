fbbbcf90df0c34fe8ec5f2df7a209c1a024317ea
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2026-02-16 23:31:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,6 +52,12 @@ tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
     git pull "$GIT_REPOSITORY_URL"
 ) || exit 1
 
+debug "Deleting contents of $tmp_dir"
+for file in $(find $tmp_dir -maxdepth 1 -type f -name '*.md' -execdir basename '{}' ';'); do
+    debug "Deleting $file"
+    rm -f "$tmp_dir/$file"
+done
+
 debug "Enumerating contents of $1"
 for file in $(find $1 -maxdepth 1 -type f -name '*.md' -execdir basename '{}' ';'); do
     debug "Copying $file"
@@ -63,7 +69,7 @@ debug "Committing and pushing changes"
     cd "$tmp_dir" || exit 1
     git add .
     git commit -m "$WIKI_COMMIT_MESSAGE"
-    git push --set-upstream "$GIT_REPOSITORY_URL" master
+    git push --set-upstream "$GIT_REPOSITORY_URL" main
 ) || exit 1
 
 rm -rf "$tmp_dir"
